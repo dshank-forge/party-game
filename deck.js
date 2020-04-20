38d1b877b5e370fca83b679bbfb2217129424944
David Shank <david@davids-mbp.home>
2020-04-19 21:23:49 -0400

    Worked on constructor() and drawCard() for the deck class

--- deck.js
+++ deck.js
@@ -1,9 +1,31 @@
+// Deck Design Plan:
+// -Give the deck a cards attribute that is a simple array of integers
+// -When a card is drawn from the deck, the deck will instantiate an object of the card class, and tie that object to the appropriate number in its array
+
 class deck {
   constructor(standard) {
     console.log("You have created a deck.");
     this.isStandard = standard;
+    this.cards = [];
+    if (this.isStandard === true) {
+      console.log("This is a standard deck.");
+      for (var counter = 1; counter <= 52; counter++) {
+        this.cards.push(counter);
+      }
+    } else {
+      console.log("This is NOT a standard deck.");
+    }
+  }
+  drawCard() {
+    console.log("You have drawn a card.");
+    return 0;
   }
 }
 
 blueDeck = new deck(true);
-console.log(blueDeck.isStandard);
+num = blueDeck.drawCard();
+console.log(num);
+
+// console.log("hello " + blueDeck.cards);
+
+// console.log(blueDeck.isStandard);
